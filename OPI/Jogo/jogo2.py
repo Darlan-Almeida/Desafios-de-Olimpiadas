c7bf91a622c4aa59e6cdaeae164214b43b413ce4
Darlan Almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-07-15 16:10:26 -0300

    Create jogo2.py
    segunda solução do problema

---
+++ OPI/Jogo/jogo2.py
@@ -0,0 +1,22 @@
+saltos = [1 , 2 , 3 , 4 , 5]
+x = int(input())
+
+jogadas = 0
+
+i = 1
+z = 0
+
+if(x <= 5):
+    print(1)
+
+else:
+
+    while( x > 0):
+        valor = saltos[-1]
+        if(x >= valor):
+            i += 1
+            jogadas += 1
+            x -= valor
+        else:
+            saltos.pop(-1)
+    print(jogadas)
