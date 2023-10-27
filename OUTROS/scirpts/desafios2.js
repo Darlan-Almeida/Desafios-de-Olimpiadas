540fe703a6be842bc29709fe6a6bb8a56723bf1e
Darlan Almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-10-27 20:08:12 +0000

    exercicia da disciplina da linguagem de script

--- OUTROS/scirpts/desafios2.js
+++ OUTROS/scirpts/desafios2.js
@@ -38,18 +38,84 @@ console.log(preco[index_fruta]) // 1)Imprima o preço da jaca.
 
 
 for([ index , fruta] of frutas.entries()){
-    console.log("Fruta: " + fruta + " valor: " + preco[index])
+    console.log("Fruta: " + fruta + " valor: " + preco[index]) //2)Imprima uma lista de todas as frutas e seus preços.
+
 }
 
+
+//3)Adicione a fruta maçã com seu preço: R$9,90.
 frutas.push("maça")
 preco.push("9,90")
 
 
 for([ index , price] of preco.entries()){
-    preco.splice(index , 1)
-    parseFloat(price.replace("," , "."))
-    preco.push(price)
+    new_price = parseFloat(price.replace("," , "."))
+    preco[index] = new_price
     
+}
+
+console.log(preco)
+
+for([index , price] of preco.entries()){
+    discount_price = price - (price * 0.1)
+    preco[index] = parseFloat(discount_price.toFixed(2)) 
+
+}
+
+console.log(preco)// 4)Aplique 10% de desconto em todas as frutas.
+
+
+//5)Remova a fruta uva.
+
+for([ index , fruta] of frutas.entries()){
+    if(fruta == "uva"){
+        frutas.splice(index , 1)
+        preco.splice(index + 1)
+
+    }
+}
+
+console.log(frutas)
+console.log(preco)
+
+
+for([index , price] of preco){
+    min_price = preco[0]
+
+    if(price < min_price){
+        min_price = price
+        index_min_price = index
+    }
+}
+
+console.log(frutas[index_min_price]) // 6)Imprima o nome da fruta mais barata.
+
+
+//7)Altere o valor da fruta banana para R$ 3,85.
+
+for([index , fruta] of frutas.entries()){
+    if(fruta == "banana"){
+        preco[index] = 3.85
+
+    }
+}
+
+console.log(frutas)
+console.log(preco)
+
+
+//8)Deixe o array de preços em ordem crescente e repita a questão 2.
+
+for([index , price] of preco.entries()){
+    for([ index2 , comparador] of preco.entries()){
+        if(price < comparador){
+            preco[index] = preco[index2]
+            preco[index2] = preco[index]
+            price = comparador
+
+        }
+    }
+
 }
 
 console.log(preco)
