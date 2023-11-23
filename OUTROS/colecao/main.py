664028f32d983085214a989b1645a62662b717f4
Darlan Almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-11-23 20:27:58 +0000

    adicionando reoluções da disciplina de programação

--- OUTROS/colecao/main.py
+++ OUTROS/colecao/main.py
@@ -65,10 +65,138 @@ def list_manipulation():
     print(list_manipulated)
 
 
-list_manipulation()
+
 
             
 
+def exponenciacion(lista):
+    print(list(map(lambda x: x ** 2, lista)))
+
+
+"""
+5 - Crie uma lista de quadrados dos números de 1 a 10 usando list comprehension.
+- Crie uma lista de números de 1-10 utilizando range
+- Utilizando list comprehension gere os números quadrados a partir da primeira lista
+"""
+
+def list_comprehension():
+    lista =[ ]
+    for i in range(11):
+        lista.append(i)
+
+    new_list = [i ** 2 for i in lista]
+
+    print(new_list)
+
+'''
+6 - Faça um programa que converta uma lista de temperaturas de Fahrenheit para Celsius, em seu programa o usuário deverá inserir uma sequência de valores que representam a temperatura em graus Fahrenheit, seu programa deve receber esses valores e armazenar em uma lista até que o valor inserido pelo usuário seja um valor em branco (uma string vazia). Converta todos os valores presentes na lista para graus Celsius usando a função map e imprima na tela em uma formatação amigável ao usuário.
+- Utilize o while e no bloco de repetição leia dados de temperatura do usuário
+- Acrescente os dados na lista
+- Crie uma função para converter temperatura Celcios para Farenheint
+- Use a função map com a função e a lista
+- Imprima todas os valores da nova lista
+'''
+
+def temperature():
+    temperature_celsius = input("type of the temperture in Celsius Scale:")
+    list_celsius = []
+    while( temperature_celsius != ""):
+        list_celsius.append(temperature_celsius)
+        temperature_celsius = input("type of the temperture in Celsius Scale:")
+
+    def celsius_to_farenheith(celsius):
+        farenheith = (float(celsius) * 1.8) * 32
+
+        return farenheith
+    
+    conversions = list(map(celsius_to_farenheith , list_celsius))
+
+    for conversion in conversions:
+        print(f"{conversion:.2f}")
+        
+"""6 - A partir do dicionário de nomes e idades de pessoas a seguir, faça um programa que imprima em ordem a partir dos nomes das pessoas, mostre a soma das idades, a média das idades e a pessoa mais velha. 
+```py
+people = {
+    "Rafael": 41,
+    "Anne": 28,
+    "Jen": 32,
+    "Satan": 2000000,
+    "Frank": 12,
+    "Sally": 19,
+    "Bob": 42,
+    "Sue": 21,
+    "Jill": 32,
+    "Jack": 32,
+}"""
+
+def employes():
+    people = {
+    "Rafael": 41,
+    "Anne": 28,
+    "Jen": 32,
+    "Satan": 20,
+    "Frank": 12,
+    "Sally": 19,
+    "Bob": 42,
+    "Sue": 210,
+    "Jill": 32,
+    "Jack": 32,
+    }
+    
+    list_order = sorted(people)
+    ages = 0
+    quantity_people = len(people)
+    people_old_ages = 0
+    people_old = ""
+    for person in list_order:
+        print(person)
+        ages +=  people[person]
+
+        if(people[person] > people_old_ages):
+            people_old = person
 
 
 
+    average = ages/ quantity_people
+
+    print(f"The sum of ages is equal: {ages}")
+    print(f"The average of ages is equal: {average}")
+    print(f"The people more old is {people_old}")
+
+
+"""
+7 - Escreva um programa em Python para calcular a soma de todos os elementos de cada tupla armazenada dentro de uma lista de tuplas.
+
+Lista original de tuplas:
+
+[(1, 2), (2, 3), (3, 4)]
+
+Soma de todos os elementos de cada tupla armazenada dentro da mencionada lista de tuplas:
+
+[3, 5, 7]
+
+Lista original de tuplas:
+
+[(1, 2, 6), (2, 3, -6), (3, 4), (2, 2, 2, 2)]
+
+Soma de todos os elementos de cada tupla armazenada dentro da mencionada lista de tuplas:
+
+[9, -1, 7, 8]
+
+"""
+
+
+def list_tupls():
+    list_origin = [(1, 2, 6), (2, 3, -6), (3, 4), (2, 2, 2, 2)]
+    list_manipulated = [ ]
+    som_tupls = None
+    for i in list_origin:
+        if(som_tupls != None):
+            list_manipulated.append(som_tupls)
+        som_tupls = 0
+        for j in i:
+            som_tupls += j 
+    list_manipulated.append(som_tupls)
+    print(list_manipulated)
+
+list_tupls()
